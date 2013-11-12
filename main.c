486f2c49b0992657096dc9eff6fc3c3b63ca2ade
zhilingyin <zhilingyin@hustunique.com>
2013-11-12 23:11:33 +0800

    migon

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>                    //深度优先
 #include <stdlib.h>
 #define H 20
-#define L 20
+#define L 21
+#include <conio.h>
+#include <windows.h>
 char a[H][L];
+const int di[4] = {0,1,0,-1},
+          dj[4] = {1,0,-1,0};
 int main()
 {
+    void print(void);
+    void dfs(int i,int j);
     int i,j,x,y;
     FILE *fp;
     fp=fopen("migong.txt","r");
@@ -13,7 +19,6 @@ int main()
         printf("无法打开文件\n");
         exit(0);
     }
-    printf("迷宫\n");
     for (i=0;i<H;i++)
     {
         for (j=0;j<L;j++)
@@ -24,58 +29,39 @@ int main()
                 x=i;
                 y=j;
             }
-            printf("%c",a[i][j]);
         }
     }
     dfs(x,y);
-    printf("路径如下\n");
-    for (i=0;j<H;i++)
+   return 0;
+}
+void print(void)
+{
+    int i,j;
+     for (i=0;i<H;i++)
     {
         for (j=0;j<L;j++)
         {
             printf("%c",a[i][j]);
         }
     }
-    return 0;
 }
 
 void dfs(int x,int y)
 {
     if(a[x][y]!='E')
     {
-        if(x+1<H)
-        {
-            if(a[x+1][y]='.')
-               {
-                   a[x+1][y]='>';
-                   dfs(x+1,y);
-               }
-        }
-        if(x-1>=0)
-        {
-             if(a[x-1][y]='.')
-                {
-                   a[x-1][y]='>';
-                   dfs(x-1,y);
-                 }
-        }
-       if(y+1<L)
+        int k;
+	for (k = 0; k < 4; k++)
+		if (x + di[k] >= 0 && y + di[k] < L
+			&& x + dj[k] >= 0 && y + dj[k] < L
+			&& a[x + di[k]][y + dj[k]] == '.')
        {
-           if(a[x][y+1]='.')
-                  {
-                       a[x][y+1]='>';
-                       dfs(x,y+1);
-                   }
-       }
-        if(y-1>=0)
-        {
-             if(a[x][y-1]='.')
-                  {
-                      a[x][y-1]='>';
-                      dfs(x,y-1);
-                  }
+           a[x + di[k]][y + dj[k]] = '>';
+           print();
+           Sleep(100);
+           system("cls");
+           dfs(x+di[k],y+dj[k]);
         }
-
     }
 }
 
